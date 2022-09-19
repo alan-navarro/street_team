3d02633f1f39b642f65463a03a0164aa6ecbb6b5
alan navarro <alannavarro444@gmail.com>
2022-09-18 21:56:20 -0500

    sfidjvcxm

--- app_framework/models/users_survey.py
+++ app_framework/models/users_survey.py
@@ -12,14 +12,16 @@ class UsersSurvey:
 
     def make_query(self):
         
-        query_preferences = sql.SQL('''SELECT * 
-                                     FROM user_preferences''')
+        # query_preferences = sql.SQL('''SELECT * 
+        #                              FROM user_preferences''')
         
-        # sql_query = pd.read_sql(query_preferences, conn)
-        result = await conn.execute(text(query_preferences))
+        # # sql_query = pd.read_sql(query_preferences, conn)
+        # result = await conn.execute(text(query_preferences))
 
-        print(result)
-
+        # print(result)
+        async with self.async_engine.connect() as con:
+            query = "SELECT * FROM user_preferences"
+            result = await con.execute(text(query))
         return result
 
 
